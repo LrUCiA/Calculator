891046525dc5bc1f2e5b130917306f36face5582
LrUCiA <crosshertz7@gmail.com>
2021-11-29 09:53:42 +0900

    Initial Commit

---
+++ CalculatorSelf/CalculatorMain.swift
@@ -0,0 +1,71 @@
+//
+//  CalculatorMain.swift
+//  CalculatorSelf
+//
+//  Created by 장명근 on 2021/11/14.
+//
+
+import Foundation
+
+class CalculatorMain {
+    var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+let operationForCalculator: Dictionary <String, KindsOfOperation> = [
+    "+": .BinaryOperation({$0+$1}),
+    "-": .BinaryOperation({$0-$1}),
+    "÷": .BinaryOperation({$0/$1}),
+    "×": .BinaryOperation({$0*$1}),
+    "+ / −": .UniOperation({-$0}),
+    "AC": .Constant(0),
+    "√": .UniOperation(sqrt),
+    "∏": .Constant(.pi),
+    "=": .Equal
+]
+
+enum KindsOfOperation {
+    case BinaryOperation((Double, Double) -> Double)
+    case UniOperation((Double) -> Double)
+    case Constant(Double)
+    case Equal
+}
+    
+    func startOperation(symbol: String) {
+        if let calculate = operationForCalculator[symbol] {
+            switch calculate {
+            case .UniOperation(let uniFunction):
+                accumulator = uniFunction(accumulator)
+            case .BinaryOperation(let biFunction):
+                clickDirectOperation()
+                saveOperation = SaveForNextOperation(nextOperation: biFunction, firstOperhand: accumulator)
+            case .Constant(let constant):
+                accumulator = constant
+            case .Equal:
+                clickDirectOperation()
+            }
+        }
+    }
+    
+    var saveOperation: SaveForNextOperation?
+    
+    struct SaveForNextOperation {
+        var nextOperation: (Double, Double) -> Double
+        var firstOperhand: Double
+    }
+    
+    func clickDirectOperation() {
+        if saveOperation != nil {
+            accumulator = saveOperation!.nextOperation(saveOperation!.firstOperhand, accumulator)
+            saveOperation = nil
+        }
+    }
+    var result: Double {
+        get {
+            return accumulator
+    }
+}
+
+}
---
+++ CalculatorSelf/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  CalculatorSelf
+//
+//  Created by 장명근 on 2021/11/13.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    var calculatorMain = CalculatorMain()
+    
+    var displayValue: Double {
+        get {
+            return Double(showDigit.text!)!
+        }
+        set{
+            showDigit.text = String(newValue)
+        }
+    }
+    var userIsTyping = false
+    
+    @IBOutlet weak var showDigit: UITextField!
+    
+    @IBAction func digitButton(_ sender: UIButton) {
+        let digit = (sender.currentTitle)!
+        if userIsTyping {
+            let textCurrentlyInDisplay = showDigit.text!
+            if textCurrentlyInDisplay.contains(".") && digit.contains("."){
+                showDigit.text = textCurrentlyInDisplay
+            }
+            else {showDigit.text = textCurrentlyInDisplay + digit}
+            
+        } else if digit.contains("."){
+            showDigit.text = "0"+digit
+        }
+        else {
+            showDigit.text = digit
+        }
+        userIsTyping = true
+    }
+    
+
+    @IBAction func operationButton(_ sender: UIButton) {
+        if userIsTyping {
+            calculatorMain.setOperand(operand: displayValue)
+        }
+        userIsTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            calculatorMain.startOperation(symbol: mathematicalSymbol)
+        }
+        displayValue = calculatorMain.result
+    }
+}
+
